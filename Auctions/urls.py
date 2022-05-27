7ec3341c413b79f386119e65fb7535cb22e58f68
Jason <jasonhsutsean@gmail.com>
2022-05-27 20:22:48 +0800

    update index.html and views.py.index

--- Auctions/urls.py
+++ Auctions/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.index, name='index'),#HOME
     path('create', views.create, name='create'),
     path('watchlist', views.watchlist, name='watchlist'),
+
+    path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+
     path('notification', views.notification, name='notification'),
+    path('product/<int:p_id>', views.product, name='product'),
 ]
--- Auctions/views.py
+++ Auctions/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render
 from django.urls import reverse
 from . import models
 
+global categories
+categories = ('Antiques', 'Art', 'Books', 'CDs, DVDs, Games', 'Clothing', 'Collectibles', 'Computers',
+				'Dining', 'Electronics', 'Food & Gourmet Items', 'For Your Pet', 'Golf & Sports Gear',
+				'Handbags', 'Health & Fitness', 'Home', 'Jewelry', 'Lawn & Garden', 'Memorabilia', 'Other',
+				'Services', 'Spa & Beauty', 'Tickets-Entertainment', 'Tickets-Sports', 'Toys', 'Travel',
+				'Unique Experiences', 'Wine')
+
 global user_id
 user_id = 1
 def user_is_authenticated():
@@ -19,25 +26,37 @@ def user_is_authenticated():
 
 def index(request):
     # Check whether user is login or not 
-    global user_id
+    global user_id, categories
+    if user_id == -1:
+        return HttpResponseRedirect(reverse("auc:login"))
     user_name = user_is_authenticated()
-   
-    
-    return render(request, 'HTML/index.html', {
-            "user_id": user_id,
-            "user_name": user_name
-            
-        })
+
+    if request.method == "GET":
+        return render(request, 'HTML/index.html', {
+                "user_id": user_id,
+                "user_name": user_name,
+                "categories": categories,
+                
+            })
+    #request.method == "POST" sort by time or category
+
 
 def create(request):
-    pass
+    if request.method == "POST":
+        pass
+    #else GET request
 
 def watchlist(request):
     pass
 
+def login(request):
+    pass
 
 def logout(request):
     pass
 
 def notification(request):
+    pass
+
+def product(request, p_id):#p_id = product_id
     pass
